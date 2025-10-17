0f39d2ac3f566f4096283d9f9204b587ed9c66f6
HikKykung <nongnewrunner2015@gmail.com>
2025-10-17 12:19:13 +0700

    fix: cum main.py and backend/requirements.txt

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI, Request
-from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
-from .filter.prompt.prompt_styple import SYSTEM_INSTRUCTION
-from .filter.models.schemas import ResponseStructor
-from .filter.gate_service import gate
-from .filter.client import get_openai_api_key
+# from fastapi.responses import JSONRespons
+# from .filter.prompt.prompt_styple import SYSTEM_INSTRUCTION
+# from .filter.models.schemas import ResponseStructor
+# from .filter.gate_service import gate
+# from .filter.client import get_openai_api_key
 
 app = FastAPI()
 
@@ -16,14 +16,14 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-API = get_openai_api_key()
+# API = get_openai_api_key()
 
 @app.get("/")
 async def root():
-    return {'status': 'ok' if API else 'error'}
+    return {"message": "ไอดำ.api"}
 
-@app.post("/filter")
-async def filter(request: Request):
-    request_json = await request.json()
-    result = gate(request_json['txt'],API)
-    return result
+# @app.post("/filter")
+# async def filter(request: Request):
+#     request_json = await request.json()
+#     result = gate(request_json['txt'],API)
+#     return result

97e309cd8c5f5530d505681ad50df90d057ffd5f
HikKykung <nongnewrunner2015@gmail.com>
2025-10-27 22:45:58 +0700

    n team

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,5 +1,8 @@
+//dirname ใช้สำหรับดึงชื่อโฟลเดอร์จาก path ของไฟล์ เช่นตัดชื่อไฟล์ออก เหลือแค่ path ของ directory
 import { dirname } from "path";
+//fileURLToPath ใช้แปลง URL ของไฟล์ (เช่น import.meta.url) ให้เป็น path จริงของไฟล์ในระบบ
 import { fileURLToPath } from "url";
+//FlatCompat ใช้เพื่อให้สามารถใช้ config แบบเก่า (.eslintrc) กับระบบ config แบบใหม่ของ ESLint (Flat Config)
 import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);

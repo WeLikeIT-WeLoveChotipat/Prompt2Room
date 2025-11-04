650306adad380a6838c5de03542f834279e0eb4f
HikKykung <nongnewrunner2015@gmail.com>
2025-11-04 23:42:19 +0700

    fix about
    fix about add footer and Drivingforce

---
+++ frontend/app/about/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+import Drivingforce from "./components/Drivingforce"
+import Footer from "../ui/footer"
+
+export default function About() {
+  return (
+    <section>
+      <Drivingforce />
+      <Footer/>
+    </section>
+  )
+}
